572dfe9d9d274ddec561318c556ee02a28f30b7f
iamakeem <bolarinwa_akeem@yahoo.com>
2023-01-26 21:38:12 +0100

    finally sorted

---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line no-undef
+const Decentraskill = artifacts.require('Decentraskill');
+
+module.exports = function (deployer) {
+  deployer.deploy(Decentraskill);
+};
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <div className='absolute inset-x-0 text-center mx-auto bg-gray-800 text-gray-300 p-2 bottom-0 '>
+      <h1>Made with ♥ by Abdulakeem</h1>
+      <a
+        href='https://github.com/Meekah1/'
+        src=''
+        className=' text-white text-xl hover:text-gray-300 absolute right-1 bottom-2 '>
+        <i className='fab fa-github'></i>
+      </a>
+    </div>
+  );
+}
+
+export default Footer;
